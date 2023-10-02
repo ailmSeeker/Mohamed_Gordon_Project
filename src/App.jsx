33bf0076041304c4dfb731dc07102c2e1d4cb90c
Mohamed Yusuf <mohamed208.y@proton.me>
2023-10-02 11:18:04 +0300

    basic vite project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Good Morning</h1>
+    </>
+  );
+}
+
+export default App;
